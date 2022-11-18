7e7ddfc4c8baeeea7d28797af68cde44807e8923
Junpark2889 <jhpark2888@naver.com>
2022-11-18 16:45:08 +0900

    개인프로젝트 거의최종

--- my.js
+++ my.js
@@ -20,3 +20,75 @@ $("nav > ul > li ").mouseenter(function(){
 $("nav > ul > li").mouseleave(function(){
   $(this).children(".submenu").stop().slideUp(200);
 });
+
+
+$(document).ready(function(){
+  $("#playButton").mouseenter(function(){
+    $(this).children("h1").addClass("moveButton")
+  })
+})
+$(document).ready(function(){
+  $("#playButton").mouseleave(function(){
+    $(this).children("h1").removeClass("moveButton")
+  })
+})
+
+$(document).ready(function(){
+  $(".backLogin").mouseenter(function (){
+    $(this).addClass("mover");
+  })
+  $(".backLogin").mouseleave(function(){
+    $(this).removeClass("mover");
+  })
+})
+
+$(document).ready(function(){
+  $(".backSign").mouseenter(function (){
+    $(this).addClass("mover");
+  })
+  $(".backSign").mouseleave(function(){
+    $(this).removeClass("mover");
+  })
+})
+
+$(document).ready(function() {
+  $(".preNotice").mouseenter(function(){
+    $(".preNotice a").text("공지사항")
+    $(".preNotice a").addClass("bold");
+  })
+  $(".preNotice").mouseleave(function(){
+    $(".preNotice a").text("")
+    $(".preNotice a").removeClass("bold");
+  })
+})
+$(document).ready(function() {
+  $(".preNews").mouseenter(function(){
+    $(".preNews a").text("뉴스")
+    $(".preNews a").addClass("bold")
+  })
+  $(".preNews").mouseleave(function(){
+    $(".preNews a").text("")
+    $(".preNews a").removeClass("bold");
+  })
+})
+$(document).ready(function() {
+  $(".preCom").mouseenter(function(){
+    $(".preCom a").text("커뮤니티")
+    $(".preCom a").addClass("bold")
+  })
+  $(".preCom").mouseleave(function(){
+    $(".preCom a").text("")
+    $(".preCom a").removeClass("bold");
+  })
+})
+$(document).ready(function() {
+  $(".preEvent").mouseenter(function(){
+    $(".preEvent a").text("이벤트")
+    $(".preEvent a").addClass("bold")
+  })
+  $(".preEvent").mouseleave(function(){
+    $(".preEvent a").text("")
+    $(".preEvent a").removeClass("bold");
+  })
+})
+
